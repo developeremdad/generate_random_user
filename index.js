e27afbe552dbd77c456f1210a48db5f772f6f2c9
developeremdad <developeremdad@gmail.com>
2022-09-02 16:51:43 +0600

    database connected

--- index.js
+++ index.js
@@ -3,20 +3,32 @@ const dbConnect = require("./utils/dbConnect");
 const app = express()
 
 const port = process.env.PORT || 5000;
-dbConnect();
+// dbConnect();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eogpx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const database = client.db("random_user");
+    const collectionUsers = database.collection('users');
+    console.log("Database connected");
+    // perform actions on the collection object
+    client.close();
+});
 
 
-app.all("*", (req,res) =>{
+app.all("*", (req, res) => {
     res.send("No route founded. please try another route.")
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Random user app listen on port: ${port}`);
 });
 
-process.on("unhandledRejection", (error) =>{
+process.on("unhandledRejection", (error) => {
     console.log(`unhandledRejection Error: Name: ${error.name} & Message: ${error.message}`);
-    app.close(()=>{
+    app.close(() => {
         process.exit(1);
     });
 });
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,14 +1,15 @@
-function dbConnect() {
-    const { MongoClient, ServerApiVersion } = require('mongodb');
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eogpx.mongodb.net/?retryWrites=true&w=majority`;
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// function dbConnect() {
+//     const { MongoClient, ServerApiVersion } = require('mongodb');
+//     const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eogpx.mongodb.net/?retryWrites=true&w=majority`;
+//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-    client.connect(err => {
-        const collection = client.db("random_user").collection("users");
-        console.log("Database connected");
-        // perform actions on the collection object
-        client.close();
-      });
-}
+//     client.connect(err => {
+//         const database = client.db("random_user");
+//         const collectionUsers = database.collection('users');
+//         console.log("Database connected");
+//         // perform actions on the collection object
+//         client.close();
+//       });
+// }
 
-module.exports = dbConnect;
+// module.exports = dbConnect;
